32b6f999310c3202534bb8f933f5d584197c1444
sujitdhamke33 <sujitdhamke12@gmail.com>
2024-03-26 15:56:07 +0530

    Pass Lambda Function to show result

--- LambdaCalculator.java
+++ LambdaCalculator.java
@@ -3,18 +3,28 @@ package LambdaExpression;
 @FunctionalInterface
 interface Mathcalculator{
     public int calculator(int a, int b);
+    static void printResult(int a,int b,String Functions,Mathcalculator obj){
+        System.out.println("Result of " + Functions + " is " + obj.calculator(a,b));
+    }
 }
 
 public class LambdaCalculator {
     public static void main(String[] args) {
-        System.out.println("Calculator using the Lambda Functions");
+        Mathcalculator sub = (a,b)-> a - b ;
+
+        Mathcalculator div = (a,b)-> a / b ;
 
         Mathcalculator add = (a,b)-> a + b ;
-        System.out.println("Addition : " + add.calculator(10,20));
-        Mathcalculator sub = (a,b)-> a - b ;
-        System.out.println("Substraction : " + sub.calculator(40,20));
-        Mathcalculator div = (a,b)-> a / b ;
-        System.out.println("Division : " + div.calculator(20,10));
+
+
+        System.out.println("Calculator using the Lambda Functions with printresult ");
+
+        Mathcalculator.printResult(100,20,"Addition",add);
+        Mathcalculator.printResult(100,20,"Substrraction",sub);
+        Mathcalculator.printResult(20,20,"Division",div);
+
+
+
 
     }
 }
