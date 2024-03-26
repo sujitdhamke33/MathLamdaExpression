cae02221f63fe01fda8be278920e4b69bf4c40e5
sujitdhamke33 <sujitdhamke12@gmail.com>
2024-03-26 15:42:21 +0530

    Addtion,substraction div perform by use of lambda

--- LambdaCalculator.java
+++ LambdaCalculator.java
@@ -9,6 +9,12 @@ public class LambdaCalculator {
     public static void main(String[] args) {
         System.out.println("Calculator using the Lambda Functions");
 
+        Mathcalculator add = (a,b)-> a + b ;
+        System.out.println("Addition : " + add.calculator(10,20));
+        Mathcalculator sub = (a,b)-> a - b ;
+        System.out.println("Substraction : " + sub.calculator(40,20));
+        Mathcalculator div = (a,b)-> a / b ;
+        System.out.println("Division : " + div.calculator(20,10));
 
     }
 }
