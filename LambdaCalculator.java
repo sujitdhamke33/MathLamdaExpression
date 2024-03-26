d00df8d5c31fa6b803598b1d346cd2b8dd548945
sujitdhamke33 <sujitdhamke12@gmail.com>
2024-03-26 15:39:55 +0530

    Master Created

---
+++ LambdaCalculator.java
@@ -0,0 +1,14 @@
+package LambdaExpression;
+
+@FunctionalInterface
+interface Mathcalculator{
+    public int calculator(int a, int b);
+}
+
+public class LambdaCalculator {
+    public static void main(String[] args) {
+        System.out.println("Calculator using the Lambda Functions");
+
+
+    }
+}
